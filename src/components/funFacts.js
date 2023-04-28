93f01ebf94817bf9d7502c2d78fd69993c85e4f4
marcus nilsson <marcusnilsson2010@gmail.com>
2023-04-28 16:37:28 +0200

    initial commit

---
+++ src/components/funFacts.js
@@ -0,0 +1,25 @@
+
+const FunFacts = ()=>{
+    return (
+        <header>
+          <img 
+             src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+             alt="new"
+             width= "70px"
+      />
+            <h1> Fakta React</h1>
+            <ul>
+                <li>  Släpptes för utvecklare 2013 </li>
+                <li> Skapades i grunden av Jordan Walke </li>
+                <li> Har över 100K stjärnor på GitHub </li>
+                <li> Sköts av Facebook </li>
+                <li> Har över 100K stjärnor på GitHub </li>
+                <li> Används av alla möjliga appar</li>
+            </ul>
+        </header>
+    )
+}
+
+console.log(FunFacts);
+
+export default FunFacts
